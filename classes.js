b53e7f636e2309cf305679543b77e750e2a92a6a
Chris Aragon <caragon.tpp@gmail.com>
2021-01-20 10:23:41 -0600

    finished

--- classes.js
+++ classes.js
@@ -29,7 +29,19 @@
   Call your class Employee and receive all the data in the constructor in the order listed above.
 */
 
-//Code Here
+class Employee {
+  constructor(first_name, last_name, email, age) {
+    this.first_name = first_name;
+    this.last_name = last_name;
+    this.email = email;
+    this.age = age;
+  }
+
+  makeWidget = function () {
+    return `${this.first_name} ${this.last_name} Widget`
+  }
+
+}
 
 
 ////////// PROBLEM 2 //////////
@@ -47,57 +59,123 @@
   Call your new class Manager
 */
 
-//Code Here
+class Manager extends Employee {
+  constructor(first_name, last_name, email, age) {
+    super(first_name, last_name, email, age)
+    this.reports = []
+  }
+
+  hire (employee) {
+    this.reports.push(employee)
+  }
+  fire (index) {
+    this.reports.splice(index, 1)
+  }
+}
 
 
 ////////// PROBLEM 3 //////////
 
 /*
-  Managers for Widget Co. get promoted when they get more employees, and get a bonus when they fire employees.
-  create a class ProgressiveManager that extends Manager.  A Progressive Manager has all of the same properties as a manager with the following additional properties:
-    - title - default 'Not a manager'
-    - bonus - default 0
-
-  When employees are hired or fired, the manager's title should be updated based on the number of reports.
-    0 reports : Not a manager
-    1-3 reports : Barely Manager
-    4-10 reports : Mostly Manager
-    11-50 reports : Manager
-    51-100 reports : Manager Plus
-    101+ reports : Bestest Manager
-
-  Everytime they fire an employee they get $100 added to their bonus.
-
-  Call your new class ProgressiveManager
+ * Managers for Widget Co. get promoted when they get more employees, and get a bonus when they fire employees.
+ * create a class ProgressiveManager that extends Manager.  A Progressive Manager has all of the same properties as a 
+ * manager with the following additional properties:
+ *   - title - default 'Not a manager'
+ *   - bonus - default 0
+*
+ * When employees are hired or fired, the manager's title should be updated based on the number of reports.
+ *   0 reports : Not a manager
+ *   1-3 reports : Barely Manager
+ *   4-10 reports : Mostly Manager
+ *   11-50 reports : Manager
+ *   51-100 reports : Manager Plus
+ *   101+ reports : Bestest Manager
+*
+ * Everytime they fire an employee they get $100 added to their bonus.
+*
+ * Call your new class ProgressiveManager
 */
 
-//Code Here
+class ProgressiveManager extends Manager {
+  constructor(first_name, last_name, email, age) {
+    super(first_name, last_name, email, age)
+    this.title = 'Not a manager'
+    this.bonus = 0
+  }
+
+  hire (employee) {
+    super.hire(employee)
+    if (this.reports.length >= 1 && this.reports.length <= 3) {
+      this.title = 'Barely Manager'
+    } else if (this.reports.length >= 4 && this.reports.length <= 10) {
+      this.title = 'Mostly Manager'
+    } else if (this.reports.length >= 11 && this.reports.length <= 50) {
+      this.title = 'Manager'
+    } else if (this.reports.length >= 51 && this.reports.length <= 100) {
+      this.title = 'Manager Plus'
+    } else if (this.reports.length >= 101) {
+      this.title = 'Bestest Manager'
+    }else if (this.reports.length === 0){
+      this.title = 'Not a manager'
+    }
+    return this.title
+  }
+ fire (index) {
+   super.fire(index)
+   return this.bonus += 100
+ }
+}
 
 
 
 ////////// PROBLEM 4 - Black Diamond //////////
 
 /*
-  Widget Co has a factory that makes widgets.
-  Factories have Machines.
-
-  Make a Machine class that takes in no parameters
-  A Machine has the following properties:
-    - widgets_made_count - default 0
-    - wear_and_tear_count - default 0
-    - needs_reboot - default false
-
-  A Machine has the following methods:
-    - makeWidgets
-        - This function takes in a number and increases widgets_made_count by that amount
-        - It also increases wear_and_tear_count by 1 for every 50
-    - fixMachine
-        - This function sets needs_reboot to true
-    - reboot
-        - This function returns an anonymous function that is called when the machine is done rebooting
-        - The anonymous function should decrease wear_and_tear_count by 10, and set needs_reboot to false
+ * Widget Co has a factory that makes widgets.
+ * Factories have Machines.
+*
+ * Make a Machine class that takes in no parameters
+ * A Machine has the following properties:
+ *   - widgets_made_count - default 0
+ *   - wear_and_tear_count - default 0
+ *   - needs_reboot - default false
+*
+ * A Machine has the following methods:
+ *   - makeWidgets
+ *       - This function takes in a number and increases widgets_made_count by that amount
+ *       - It also increases wear_and_tear_count by 1 for every 50
+ *   - fixMachine
+ *       - This function sets needs_reboot to true
+ *   - reboot
+ *       - This function returns an anonymous function that is called when the machine is done rebooting
+ *       - The anonymous function should decrease wear_and_tear_count by 10, and set needs_reboot to false
 */
 
-//Code Here
+class Machine {
+  constructor (){
+    this.widgets_made_count = 0;
+    this.wear_and_tear_count = 0;
+    this.needs_reboot = false
+  }
+
+  makeWidgets = (num) =>{
+    this.widgets_made_count += num
+    if (this.widgets_made_count % 50 === 0){
+      this.wear_and_tear_count = this.widgets_made_count / 50
+    }
+  }
+
+  fixMachine = () =>{
+    this.needs_reboot = true
+  }
+
+  reboot = () =>{
+    
+    return () =>{
+      this.needs_reboot = false
+      this.wear_and_tear_count -= 10 
+    } 
+  }
+}
 
 
